2a0402418c2c23dba53b0e36a2266f7846f8578e
farhan <farhanrizqi008@gmail.com>
2023-11-21 21:28:37 +0700

    fixing import

--- src/app/pages/bookings/page.jsx
+++ src/app/pages/bookings/page.jsx
@@ -2,7 +2,7 @@
 import React, { useState } from "react";
 import Navbar from "../../components/Navbar";
 import Footer from "../../components/Footer";
-import Image from "next/image alt='Image'";
+import Image from "next/image";
 import dummy from "../../../../public/images/dummyImg.jpg";
 import InputForm from "@/app/components/InputForm";
 
